e81bafb8de60f4d47bf50e42d4ac80bc51b6a9ff
Margui <margui.berjon@gmail.com>
2020-08-04 14:07:10 +0200

    cleanning react app

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/header.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Margarita Berjón
+        </h1>
+        <h2> UX | Frontend developer</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
