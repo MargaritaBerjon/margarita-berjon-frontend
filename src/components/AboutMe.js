0da388d7d68e07be591fb4cf2bf8d80924f78c67
Margui <margui.berjon@gmail.com>
2020-08-06 21:41:17 +0200

    knowlege added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,8 +8,21 @@ function AboutMe() {
     <div className="about-me">
       <img src={MargaritaBerjon} alt="Foto de Margarita Berjon" className='about-me-foto'></img>
       <div>
-        <h1 className='about-me-h1'>Sobre mí</h1>
+        <h2 className='about-me-h2'>Sobre mí</h2>
         <p className='about-me-p'>Marketiniana digital reconvertida a frontend developer con conocimientos en diseño de experiencia de usuario y ganas de continuar aprendiendo. Mi experiencia y conocimientos en estas tres áreas me aportan una visión completa del proceso de desarrollo de productos digitales.</p>
+        <h2 className='about-me-h2'>Conocimientos</h2>
+        <div className='knowledge'>
+          <div className='knowledge-circle'>Inglés</div>
+          <div className='knowledge-circle'>HTML5</div>
+          <div className='knowledge-circle'>CSS</div>
+          <div className='knowledge-circle'>SASS</div>
+          <div className='knowledge-circle'>JavaScript</div>
+          <div className='knowledge-circle'>React</div>
+          <div className='knowledge-circle'>SCRUM</div>
+          <div className='knowledge-circle'>GIT</div>
+          <div className='knowledge-circle'>Sketch</div>
+          <div className='knowledge-circle'>Invision</div>
+        </div>
       </div>
     </div>
   );
