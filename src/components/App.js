ceeee1ebcdb9337b8d8cb53648981b48468570a8
Margui <margui.berjon@gmail.com>
2020-08-11 11:11:54 +0200

    style: mobile style first

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { getProjects } from '../services/API.js';
 import Poster from './Poster.js';
 import AboutMe from './AboutMe.js';
-import MainProjects from './MainProjects.js'
-import Experience from './Experience/Experience.js';
-import Contact from './Contact.js';
-import Footer from './Footer.js';
+// import MainProjects from './MainProjects.js'
+// import Experience from './Experience/Experience.js';
+// import Contact from './Contact.js';
+// import Footer from './Footer.js';
 import '../style/App.css';
 
 function App() {
@@ -22,10 +22,10 @@ function App() {
     <div className="App">
       <Poster></Poster>
       <AboutMe></AboutMe>
-      <MainProjects repos={projects} ></MainProjects>
-      <Experience></Experience>
-      <Contact></Contact>
-      <Footer></Footer>
+      {/* <MainProjects repos={projects} ></MainProjects> */}
+      {/* <Experience></Experience> */}
+      {/* <Contact></Contact> */}
+      {/* <Footer></Footer> */}
     </div>
   );
 }
