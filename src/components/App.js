fc09a59e4e6220f7564cdb87d5c9e14cdc53c0cd
Margui <margui.berjon@gmail.com>
2020-08-18 12:19:41 +0200

    feature: arrow donw

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,26 @@ import { getProjects } from '../services/API.js';
 import Poster from './Poster.js';
 import AboutMe from './AboutMe.js';
 import MainProjects from './MainProjects.js'
-import Experience from './Experience/Experience.js';
+// import Experience from './Experience/Experience.js';
 import Contact from './Contact.js';
 import Footer from './Footer.js';
 import '../style/App.css';
 
 function App() {
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
+  const [animationStartArrow, setAnimationStartArrow] = useState();
+  const [posterStyle, setPosterStyle] = useState({});
+  const [visibility, setVisibility] = useState('hide');
+
+
+  useEffect(() => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }, []);
+
 
   useEffect(() => {
     getProjects().then(projects => {
@@ -18,14 +31,31 @@ function App() {
 
   }, []);
 
+
+  function start() {
+    setPosterStyle({
+      transition: '2s',
+      opacity: 0,
+      height: '0px'
+    });
+    setAnimationStartArrow('hide');
+    setVisibility('view')
+  };
+
+
+
+
   return (
     <div className="App">
-      <Poster></Poster>
-      <AboutMe></AboutMe>
-      <MainProjects repos={projects} ></MainProjects>
-      <Experience></Experience>
-      <Contact></Contact>
-      <Footer></Footer>
+      <Poster start={start} animationStartArrow={animationStartArrow} posterStyle={posterStyle}></Poster>
+      <div className={visibility}>
+        <AboutMe></AboutMe>
+        <MainProjects repos={projects} ></MainProjects>
+        {/* <Experience></Experience> */}
+        <Contact></Contact>
+        <Footer></Footer>
+
+      </div>
     </div>
   );
 }
--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../style/Poster.css';
+import * as arrow from '../images/arrow.png'
 
 
-function Poster() {
+function Poster(props) {
   return (
-    <div className="poster">
+    <div className="poster" style={props.posterStyle} >
       <h1 className='poster-h1'>Margarita Berjón</h1>
       <h2 className='poster-h2'> UX | Frontend developer</h2>
+      <img src={arrow} alt="arrow" onClick={props.start} className={'startArrow'} />
     </div>
   );
 }
