7a9b590d5f970b3a812a4145f79a2b564fbcaaa3
Margui <margui.berjon@gmail.com>
2020-08-25 13:16:01 +0200

    Update docs

---
+++ docs/precache-manifest.bc73c1c7450c63c6ac36efee996275a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f815b265a43f4f63db05aea2c69082",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92009fdd771a70940226",
+    "url": "./static/css/main.45f4a1a9.chunk.css"
+  },
+  {
+    "revision": "805ad761f4d38b72f64b",
+    "url": "./static/js/2.fb36d61a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fb36d61a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92009fdd771a70940226",
+    "url": "./static/js/main.d1fdb70b.chunk.js"
+  },
+  {
+    "revision": "3bc3dbb24a5e94b74af8",
+    "url": "./static/js/runtime-main.26b300ba.js"
+  },
+  {
+    "revision": "28885d5b1a972010e9d0f954b5f5fd0d",
+    "url": "./static/media/CV-Margarita-Berjon-Frontend.28885d5b.pdf"
+  },
+  {
+    "revision": "00eff8957493b7a5711cd0686dec9d46",
+    "url": "./static/media/Margarita_Berjon_web.00eff895.jpg"
+  },
+  {
+    "revision": "18dfcb0ae4251fea23017c39cb48f33b",
+    "url": "./static/media/descargar.18dfcb0a.svg"
+  },
+  {
+    "revision": "62fcdb9bb1575a4137441a7e2c4aeb65",
+    "url": "./static/media/github.62fcdb9b.svg"
+  },
+  {
+    "revision": "90aa50c6a85986df27b86e7742163e50",
+    "url": "./static/media/linkedin.90aa50c6.svg"
+  },
+  {
+    "revision": "b8b5e3f2ceae9003a696ca24ddad15d7",
+    "url": "./static/media/mail.b8b5e3f2.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ecf0906158135185614a974cf3fba48.js"
+  "./precache-manifest.bc73c1c7450c63c6ac36efee996275a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
